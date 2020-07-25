c51f7c39c1731ac45df2369fbbbef1db29452b65
Brennan Giles <collegegiles@gmail.com>
2020-07-25 01:27:47 -0700

    SAVED v0.3
    GUI implementation! WOO! Still needs to be merged with old logic but it's been so much progress I thought it was worth pushing

--- main.py
+++ main.py
@@ -1,26 +1,194 @@
-import os
+from tkinter import *
+import tkinter as tk
+import tkinter.scrolledtext as tkst
+import time
 
-#------------------Imports and File Reading----------------------------------------
-story_sections = []
+#--------------------------------------------------------Globals--------------------------------------------------------
 
-for i in range(0,8):
-    num = i
-    story_sections.append("arc1_" + f"{num}")
+#global values for the speed and size of the text
+txtSpeed = 0.05
+txtSize = 16
 
-print(story_sections)
-
-story_content = {}
-hint = False
-for section in story_sections:
-    file_path = "script/" + section + ".txt"
-    with open(file_path, encoding="utf8") as file_reader:
-        story_content[section] = file_reader.read()
-
-
-#---------------------Globals and Flags---------------------------------------------
 food = "null"
 pName = "null"
-#---------------------------Chapters------------------------------------------------
+
+#!!!Flags!!!
+txtdone = 0
+#-------------------------------------------------------Functions-------------------------------------------------------
+def loadGame():
+    return
+
+def saveGame():
+    return
+
+def changeSettings(newSpeed, newSize):
+    global txtSpeed
+    global txtSize
+
+    if newSpeed == "slow":
+        txtSpeed = 0.2
+    if newSpeed == "standard":
+        txtSpeed = 0.05
+    if newSpeed == "fast":
+        txtSpeed = 0.01
+
+    if newSize == "small":
+        txtSize = 10
+    if newSize == "standard":
+        txtSize = 16
+    if newSize == "large":
+        txtSize = 24
+
+    textWindow.config(font=("Calibri", txtSize))
+
+def click_choice(choice):
+    disp_txt(str(txtSpeed), txtSpeed)
+
+def disp_txt(string, speed):
+    for char in string:
+        textWindow.insert(tk.INSERT, char)
+        textWindow.update()
+        time.sleep(speed)
+    global txtdone
+    txtdone=1
+
+def queue_logic(section, prompt_text,  path_a, path_b, path_c, path_d, path_e):
+    clear_screen()
+    to_display = replace_variables(story_content[section])
+    print(to_display)
+    user_input = input(prompt_text)
+    if user_input == '1':
+        path_a()
+    if user_input == '2':
+        path_b()
+    if user_input == '3':
+        if path_c != "null":
+            path_c()
+        else:
+            print("Please select a valid option \n")
+            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
+    if user_input == '4':
+        if path_d != "null":
+            path_d()
+        else:
+            print("Please select a valid option \n")
+            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
+    if user_input == '5':
+        if path_e != "null":
+            path_e()
+        else:
+            print("Please select a valid option \n")
+            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
+    else:
+        print("Please select a valid option \n")
+        queue_logic(section, prompt_text,  path_a, path_b, path_c, path_d, path_e)
+
+def replace_variables(string):
+    global food
+    global pName
+    string = string.replace("$food", food)
+    string = string.replace("$pName", pName)
+    return string
+
+
+def clear_screen():
+    textWindow.delete(1.0, END)
+    textWindow.update()
+
+#--------------------------------------------------------GUI------------------------------------------------------------
+def settingsconfig():
+    #creating settings window
+    settings_window = Toplevel()
+    settings_window.title("Settings Configuration")
+    settings_window.iconbitmap('./treelarge_CKX_icon.ico')
+    settings_window.geometry("360x200")
+    settings_window.resizable(False, False)
+
+    # putting in the defaults
+    global txtSize
+    global txtSpeed
+    speed = StringVar()
+    textSize = StringVar()
+
+    if txtSpeed == 0.01:
+        speed.set("fast")
+    if txtSpeed == 0.05:
+        speed.set("standard")
+    if txtSpeed == 0.2:
+        speed.set("slow")
+
+    if txtSize == 24:
+        textSize.set("large")
+    if txtSize == 16:
+        textSize.set("standard")
+    if txtSize == 10:
+        textSize.set("small")
+    #menu
+    loadButton = Button(settings_window, text="Load Game + load info", command=loadGame).grid(row=1, column=1)
+    saveButton = Button(settings_window, text="Save Game", command=saveGame).grid(row=2, column=1)
+    speedLabel = Label(settings_window, text="Text Scroll Speed").grid(row=3, column=1)
+    radio1 = Radiobutton(settings_window, text="Slow", variable=speed, value="slow").grid(row=4, column=0)
+    radio2 = Radiobutton(settings_window, text="Standard", variable=speed, value="standard").grid(row=4, column=1)
+    radio3 = Radiobutton(settings_window, text="Fast", variable=speed, value="fast").grid(row=4, column=2)
+    sizeLabel = Label(settings_window, text="Text Size (My Require you resize Window)").grid(row=5, column=1)
+    radio4 = Radiobutton(settings_window, text="Small", variable=textSize, value="small").grid(row=6, column=0)
+    radio5 = Radiobutton(settings_window, text="Standard", variable=textSize, value="standard").grid(row=6, column=1)
+    radio6 = Radiobutton(settings_window, text="Large", variable=textSize, value="large").grid(row=6, column=2)
+    finishButton = Button(settings_window, text="Apply Changes", command=lambda: changeSettings(speed.get(), textSize.get()))
+    finishButton.grid(row=8, column=1)
+
+def create_choices(number):
+    #create buttons for the amount of options available, represtented by 'number'
+    if number == 0:
+        option0 = Button(frame2, text="Continue...", command=lambda: click_choice(0),bg = "#333333", fg = "#EEEEEE")
+        option0.pack(fill='both', expand='yes')
+    if number > 0:
+        option1 = Button(frame2, text="fillme", command=lambda: click_choice(1),bg = "#333333", fg = "#EEEEEE")
+        option1.pack(fill='both', expand='yes')
+    if number > 1:
+        option2 = Button(frame2, text="fillme", command=lambda: click_choice(2),bg = "#333333", fg = "#EEEEEE")
+        option2.pack(fill='both', expand='yes')
+    if number > 2:
+        option3 = Button(frame2, text="fillme", command=lambda: click_choice(3),bg = "#333333", fg = "#EEEEEE")
+        option3.pack(fill='both', expand='yes')
+    if number > 3:
+        option4 = Button(frame2, text="fillme", command=lambda: click_choice(4),bg = "#333333", fg = "#EEEEEE")
+        option4.pack(fill='both', expand='yes')
+    if number > 4:
+        option5 = Button(frame2, text="fillme", command=lambda: click_choice(5),bg = "#333333", fg = "#EEEEEE")
+        option5.pack(fill='both', expand='yes')
+
+#build main GUI
+win = tk.Tk()
+win.geometry("600x800")
+win.title("testing")
+win.iconbitmap('./treelarge_CKX_icon.ico')
+#Text Frame
+frame1 = tk.Frame(
+    master = win,
+    bg = "#696969"
+)
+frame1.pack(fill='both', expand='yes')
+#Text Window
+textWindow = tkst.ScrolledText(
+    master = frame1,
+    wrap   = tk.WORD,
+    bg = "#333333",
+    fg = "#EEEEEE"
+)
+textWindow.config(font=("Calibri", txtSize))
+textWindow.pack(padx=5, pady=5, fill=tk.BOTH, expand=True,)
+
+#Button Frame
+frame2 = tk.Frame(
+    master = win,
+    bg = "#333333",
+)
+frame2.pack(fill='both', expand='yes')
+settings = Button(frame2, text="Settings", command=settingsconfig,bg = "#333333", fg = "#EEEEEE")
+settings.pack(side=RIGHT, fill=Y)
+
+#----------------------------------------------Story Sections----------------------------------------------------------
 def queue_start_story():
     clear_screen()
     to_display = replace_variables(story_content["arc1_0"])
@@ -109,56 +277,24 @@ def arc1_7():
 
     input("Press Enter to continue...")
     arc1_1()
+#-----------------------------------------------Program Start----------------------------------------------------------
+
+story_sections = []
+for i in range(0,8):
+    num = i
+    story_sections.append("arc1_" + f"{num}")
+
+story_content = {}
+for section in story_sections:
+    file_path = "script/" + section + ".txt"
+    with open(file_path, encoding="utf8") as file_reader:
+        story_content[section] = file_reader.read()
+
+#-----------------------------------------------------MAIN-------------------------------------------------------------
 
 
-#----------------------Logic----------------------------------------------------
+stringy = "this is an example used for testing purposes"
+create_choices(0)
+disp_txt(stringy, txtSpeed)
 
-def queue_logic(section, prompt_text,  path_a, path_b, path_c, path_d, path_e):
-    clear_screen()
-    to_display = replace_variables(story_content[section])
-    print(to_display)
-    user_input = input(prompt_text)
-    if user_input == '1':
-        path_a()
-    if user_input == '2':
-        path_b()
-    if user_input == '3':
-        if path_c != "null":
-            path_c()
-        else:
-            print("Please select a valid option \n")
-            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
-    if user_input == '4':
-        if path_d != "null":
-            path_d()
-        else:
-            print("Please select a valid option \n")
-            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
-    if user_input == '5':
-        if path_e != "null":
-            path_e()
-        else:
-            print("Please select a valid option \n")
-            queue_logic(section, prompt_text, path_a, path_b, path_c, path_d, path_e)
-    else:
-        print("Please select a valid option \n")
-        queue_logic(section, prompt_text,  path_a, path_b, path_c, path_d, path_e)
-
-def replace_variables(string):
-    global food
-    global pName
-    string = string.replace("$food", food)
-    string = string.replace("$pName", pName)
-    return string
-
-
-def clear_screen():
-    clear = lambda: os.system('cls' if os.name == 'nt' else 'clear')
-    clear()
-
-def main():
-    queue_start_story()
-
-
-if __name__ == '__main__':
-    main()
+win.mainloop()
